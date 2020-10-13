ab2ec4b540779ead211e569c8fc0d0870fb85ae9
AdamGonda <adamgondagyula@gmail.com>
2020-10-13 19:46:26 +0200

    ci

---
+++ hello.js
@@ -0,0 +1,22 @@
+const path = require('path'),
+  fs = require('fs')
+
+function fromDir(startPath, filter) {
+  if (!fs.existsSync(startPath)) {
+    console.log('no dir ', startPath)
+    return
+  }
+
+  const files = fs.readdirSync(startPath)
+  for (const i = 0; i < files.length; i++) {
+    const filename = path.join(startPath, files[i])
+    const stat = fs.lstatSync(filename)
+    if (stat.isDirectory()) {
+      fromDir(filename, filter) //recurse
+    } else if (filename.indexOf(filter) >= 0) {
+      console.log('-- found: ', filename)
+    }
+  }
+}
+
+fromDir('.', '.html')
