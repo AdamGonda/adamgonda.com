03ac6addb4a76c327c79f82eabfb015c7426de6d
AdamGonda <adamgondagyula@gmail.com>
2020-10-14 07:21:13 +0200

    find all relevant html files for modification

--- hello.js
+++ hello.js
@@ -2,16 +2,14 @@ const glob = require('glob')
 const fs = require('fs')
 
 glob(__dirname + '/**/*.html', {}, (err, files) => {
-  const filter = file =>
-    !file.includes('_includes') && !file.includes('_layouts')
+  const filter = file => file.includes('_site')
 
-  files.filter(filter).forEach(root => {
-    
-    fs.readFile(root, 'utf8', function (err, data) {
+  files.filter(filter).forEach((root, idx) => {
+    fs.readFile(root, 'utf8', function (err, html) {
       if (err) {
         return console.log(err)
       }
-      console.log(data)
+
     })
   })
 })
