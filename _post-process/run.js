6a82fcf675b3f2b8a4867f9e7893bd7efb8d4090
AdamGonda <adamgondagyula@gmail.com>
2020-10-19 12:35:33 +0200

    leftout

--- _post-process/run.js
+++ _post-process/run.js
@@ -1,11 +1,9 @@
 const { asyncPipe } = require('./utils')
 const PIL = require('./add-progressive-image-loading')
-const compression = require('./add-image-compression')
 
 // prettier-ignore
 asyncPipe({}) (
   PIL.apply,
-  // compression.apply,
 )
 
 
