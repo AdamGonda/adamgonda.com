1c714ba7b9f9f70244de20c595e85d051663e416
adamgonda <adamgondagyula@gmail.com>
2022-06-11 18:31:11 +0200

    add get-all-tags-from-posts.js

--- _post-process/run.js
+++ _post-process/run.js
@@ -1,9 +1,9 @@
 const { asyncPipe } = require('./utils')
 const PIL = require('./add-progressive-image-loading')
+const GET_TAGS = require('./get-all-tags-from-posts')
 
 // prettier-ignore
 asyncPipe({}) (
   PIL.apply,
+  GET_TAGS.apply,
 )
-
-
