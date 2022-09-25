5b6bb88f937deeced6ed570c89094278ba0f7ab8
adamgonda <adamgondagyula@gmail.com>
2022-09-25 08:11:37 +0200

    fix easter egg link when on post page

--- assets/js/easter-egg.js
+++ assets/js/easter-egg.js
@@ -4,7 +4,7 @@
 	function showEasterEgg() {
 		const nameLogo = document.querySelector('nav a')
 		nameLogo.innerHTML = '[ Easter 🐣 ]'
-		nameLogo.href = 'easter-egg'
+		nameLogo.href = '/easter-egg'
 	}
 
   const randomNumber = Math.random() * 100
