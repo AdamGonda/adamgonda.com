7e88ef0abe41672ca1ab6c4b9dd6fa461c12810c
adamgonda <adamgondagyula@gmail.com>
2022-12-31 11:05:12 +0100

    refactor

--- assets/js/navigator.js
+++ assets/js/navigator.js
@@ -7,17 +7,6 @@ document.querySelectorAll('.navigator button').forEach((button) => {
   button.addEventListener('click', handleClickLink);
 });
 
-let observer1 = new IntersectionObserver(function(entries) {
-	if(entries[0].isIntersecting === true)
-		console.log('3 is fully visible in screen');
-}, { threshold: [1] });
-
-let observer2 = new IntersectionObserver(function(entries) {
-	if(entries[0].isIntersecting === true)
-		console.log('2 is fully visible in screen');
-}, { threshold: [1] });
-
-
 const observers = []
 for (let i = 0; i < 4; i++) {
   let observer = new IntersectionObserver(function(entries) {
