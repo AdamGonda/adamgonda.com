413be7bdf487e5850cae6de6d06059c600cfef1d
adamgonda <adamgondagyula@gmail.com>
2023-01-03 05:40:17 +0100

    u

--- assets/js/tabs.js
+++ assets/js/tabs.js
@@ -1,10 +1,23 @@
 // set active tab id
+// just show idx 0 tab first
 
-Array.from(document.querySelectorAll('.tabs')).forEach((tabs) => {
-  Array.from(tabs.children).forEach((tab, idx) => tab.addEventListener('click', () => handleClick(tabs, idx)))
+const tabs = Array.from(document.querySelectorAll('.tabs'))
+const tabsContent = Array.from(document.querySelectorAll('.tabs-content'))
+
+tabsContent.forEach(tabsContent => {
+	Array.from(tabsContent.children).forEach((content, idx) => {
+		if (idx > 0) {
+			content.style.display = 'none'
+		}
+	})
+})
+
+tabs.forEach(tabs => {
+	Array.from(tabs.children).forEach((tab, idx) => {
+		tab.addEventListener('click', () => handleClick(tabs, idx))
+	})
 })
 
 function handleClick(tabs, idx) {
-  tabs.dataset.active = idx
-  console.log(idx);
+	tabs.dataset.active = idx
 }
