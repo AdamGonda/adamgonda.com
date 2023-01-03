fcb4fdc65b12ef69f775e267892e030a00ecc42c
adamgonda <adamgondagyula@gmail.com>
2023-01-03 07:11:58 +0100

    u

--- assets/js/tabs.js
+++ assets/js/tabs.js
@@ -12,15 +12,18 @@ tabCompBodys.forEach(body => {
 
 tabCompHeaders.forEach(header => {
 	Array.from(header.children).forEach((child, idx) => {
-		child.addEventListener('click', () => handleClick(header, idx))
+		child.addEventListener('click', () => handleClick(header, child, idx))
 	})
 })
 
-function handleClick(header, idx) {
+function handleClick(header, child, childIdx) {
+  Array.from(header.children).forEach(child => child.classList.remove('active'))
+  child.classList.add('active')
+
   tabCompBodys.forEach(body => {
     if(body.dataset.id == header.dataset.id){
-      Array.from(body.children).forEach((child, i) => {
-        if (i == idx) {
+      Array.from(body.children).forEach((child, idx) => {
+        if (idx == childIdx) {
           child.style.display = 'block'
         } else {
           child.style.display = 'none'
